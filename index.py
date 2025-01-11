b9824367783c84672b7d8acfc6ff0399cde58f64
SmillaRB <tiny.angel.063@gmail.com>
2025-01-11 14:32:46 -0500

    document indexer functions

--- index.py
+++ index.py
@@ -24,6 +24,19 @@ class DocumentIndexer:
         texto = re.sub(r'[\W_]+', ' ', texto.lower()) 
         palabras = texto.split()
         return [palabra for palabra in palabras if palabra not in self.stop_words]
+   
+    def calcular_umbral(self):
+        """Calcula el umbral dinámico basado en la frecuencia de las palabras en todo el corpus."""
+        palabra_frecuencia = defaultdict(int)
+        for palabras in self.Larousse.values():
+            for doc, freq in palabras.items():
+                palabra_frecuencia[doc] += freq
+
+        total_frecuencias = sum(palabra_frecuencia.values())
+        if len(palabra_frecuencia) > 0: 
+            self.threshold = total_frecuencias / len(palabra_frecuencia)
+        else:
+            self.threshold = float('inf') 
 
     def cargar_documentos(self, ruta_carpeta):
         for archivo in os.listdir(ruta_carpeta):
@@ -32,3 +45,20 @@ class DocumentIndexer:
                 with open(ruta_txt, 'r', encoding='utf-8') as file:  
                     texto = file.read()
                     self.documentos[archivo] = texto
+
+    def construir_indices(self):
+        total_documentos = len(self.documentos)
+        for doc, texto in self.documentos.items():
+            palabras = self.limpiar_texto(texto)
+            total_palabras = len(palabras)
+            frecuencias = defaultdict(int)
+
+            for palabra in palabras:
+                frecuencias[palabra] += 1
+
+            for palabra, frecuencia in frecuencias.items():
+                self.Larousse[palabra][doc] = frecuencia
+
+            for palabra, frecuencia in frecuencias.items():
+                if frecuencia <= self.threshold:  # Excluir palabras muy comunes
+                    self.TF_IDF[palabra][doc] = (frecuencia / total_palabras) * math.log10(total_documentos / len(self.Larousse[palabra]))
