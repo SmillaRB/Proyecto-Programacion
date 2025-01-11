551a24cad745254deac78c61a8ddacffebb2552b
SmillaRB <tiny.angel.063@gmail.com>
2025-01-10 22:53:07 -0500

    stopwords added

---
+++ index.py
@@ -0,0 +1,26 @@
+import os
+import re
+import math
+from collections import defaultdict
+
+class DocumentIndexer:
+    def __init__(self):
+        self.Larousse = defaultdict(dict)
+        self.TF_IDF = defaultdict(dict)
+        self.documentos = {}
+        self.threshold = None
+        self.stop_words = set() 
+
+    def cargar_stop_words(self, ruta_archivo):
+        """Carga las palabras no deseadas desde un archivo."""
+        try:
+            with open(ruta_archivo, 'r', encoding='utf-8') as file:
+                self.stop_words = set(file.read().splitlines())
+        except Exception as e:
+            print(f"Error cargando las stop words: {e}")
+
+    def limpiar_texto(self, texto):
+        """Elimina puntuaciones, pasa a minúsculas y excluye palabras no deseadas."""
+        texto = re.sub(r'[\W_]+', ' ', texto.lower()) 
+        palabras = texto.split()
+        return [palabra for palabra in palabras if palabra not in self.stop_words]
