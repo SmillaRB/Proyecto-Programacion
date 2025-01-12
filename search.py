45a61b60c7c2bbf6f4cb4daeef843fae95cc306f
SmillaRB <tiny.angel.063@gmail.com>
2025-01-11 21:23:14 -0500

    snippets for occurrences

--- search.py
+++ search.py
@@ -4,7 +4,44 @@ from collections import defaultdict
 class SearchEngine:
     def __init__(self, indexer):
         self.indexer = indexer
+    
+    def obtener_snippet(self, documento, palabras_query):
+        """Encuentra el fragmento más relevante del documento para las palabras de la consulta."""
+        texto = self.indexer.documentos[documento]
+        palabras_documento = texto.split()
 
+        max_relevancia = 0
+        mejor_inicio = 0
+        mejor_fin = 0
+
+        ventana_tamano = 30  
+        palabras_posiciones = [i for i, palabra in enumerate(palabras_documento) if palabra in palabras_query]
+
+        if not palabras_posiciones:
+            return texto[:200] 
+
+        for i in palabras_posiciones:
+            inicio = max(0, i - ventana_tamano // 2)
+            fin = min(len(palabras_documento), i + ventana_tamano // 2)
+            ventana = palabras_documento[inicio:fin]
+
+            relevancia = sum(1 for palabra in palabras_query if palabra in ventana)
+
+            if relevancia > max_relevancia:
+                max_relevancia = relevancia
+                mejor_inicio = inicio
+                mejor_fin = fin
+
+        snippet = " ".join(palabras_documento[mejor_inicio:mejor_fin])
+
+        punto_inicio = texto.find(snippet)
+        if punto_inicio != -1:
+            punto_fin = texto.find(".", punto_inicio + len(snippet))  
+            if punto_fin != -1:
+                snippet = texto[punto_inicio:punto_fin + 1]
+
+        return snippet
+    
     def query(self, query):
         palabras_query = self.indexer.limpiar_texto(query)  
         scores = defaultdict(float)
